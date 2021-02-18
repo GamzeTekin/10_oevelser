a3948af79b7e299d3030a0c38d0faa7c6921af82
GamzeTekin <73933082+GamzeTekin@users.noreply.github.com>
2021-02-18 07:31:54 +0100

    Add files via upload
    Oevelse 48 WPF

---
+++ _48WPF/_48/APIhelper.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _48
+{
+    public static class APIhelper
+    {
+        static dynamic APIProcess(string url) 
+        {
+            var webRequest = WebRequest.Create(string.Format(@"{0}", url));
+
+            using (var response = webRequest.GetResponse())
+            using (var content = response.GetResponseStream())
+            using (var reader = new StreamReader(content))
+            {
+                return reader.ReadToEnd();
+            }
+        }
+
+        public static string reader(string city)
+        {
+            string content = APIProcess($"http://api.openweathermap.org/data/2.5/weather?q={city}&units=metric&appid=73c9d2f3fa70a94f702677efafd617ad");
+            var temp = content.Substring(content.IndexOf("temp")).Split(',')[0].Split(':')[1];
+           
+            return temp; 
+        }
+    }
+
+
+}
